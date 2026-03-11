efc5f89d39de6bd250fcd369f4ce9f17f806dc6f
qin2007 <qin@ractoc.com>
2026-03-11 14:17:20 +0100

    indexed update

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
-<?php
+<?php use ANTHeader\ANTNavIStyle;
 use function ANTHeader\ANTNavFavicond;
+use function Helpers\htmlspecialchars12;
 use function ANTHeader\ANTNavBinary;
 use function ANTHeader\create_head2;
-use function Helpers\htmlspecialchars12;
+use ANTHeader\ANTNavLinkTag;
 
 require_once "{$_SERVER['DOCUMENT_ROOT']}/require/createHead2.php";
 create_head2($title = 'ANTRequest\'s Technical Specification', [
         'base' => '/standard/',
-], [], [ANTNavFavicond('https://ANTRequest.nl', 'ANT\'s Gallery'),
+], [new ANTNavLinkTag('stylesheet', '/gallery/ddDL-table.css'),
+        new ANTNavLinkTag('stylesheet', ['/standard/layerzip.css', '/standard/w3sWarnings.css']),
+        new ANTNavIStyle('.warnbox{margin-bottom:0}.table{overflow-x:scroll;margin-top:1em}'),
+], [ANTNavFavicond('https://ANTRequest.nl', 'ANT\'s Gallery'),
         ANTNavBinary('https://ANTRequest.nl', $title, true)]);
 
 $array = array();
@@ -32,16 +36,70 @@ foreach ($array as $item) {
     <h2>Specifications</h2>
     <div><?= (function () use ($array) {
             $result = '';
+            $matches = json_decode(file_get_contents(__DIR__ . '/lastModified.json'), true)['metadata'];
             foreach ($array as $slug => $item) {
                 $slugHTML = htmlspecialchars12($slug);
-                $result .= "<h3 id=\"$slugHTML\">$slugHTML</h3><ul>";
+                $result .= "<h3 id=\"$slugHTML\">$slugHTML</h3><div class=table><table><thead><tr><th scope=col>Version" .
+                        "<th scope=col>Last Modified<th scope=col>Warnings<th scope=col>Alternate Downloads</thead><tbody>";
+
                 foreach ($item as $version) {
                     $versionHTML = "{$version['major']}.{$version['minor']}.{$version['patch']}";
-                    $result .= "<li><a href=$slugHTML/$versionHTML/>$versionHTML</a>";
+                    $major = $version['major'];
+                    $minor = $version['minor'];
+                    $patch = $version['patch'];
+                    $mtime = gmdate('Y-m-d\\TH:i:s\\Z',
+                            filemtime(__DIR__ . "/$slug/$major.$minor.$patch/index.php"));
+                    $warning = array();
+                    if ($matches[$slug]) {
+                        if ($matches[$slug]["$major.$minor.$patch"]) {
+                            if ($matches[$slug]["$major.$minor.$patch"]['lastModified']) {
+                                $mtime = $matches[$slug]["$major.$minor.$patch"]['lastModified'];
+                            }
+                            $array = $matches[$slug]["$major.$minor.$patch"];
+                            $warnArray = $array['warning'];
+                            if ($warnArray['warningLevel']) {
+                                $warning['warningLevel'] = match ($warnArray['warningLevel']) {
+                                    'warning' => 'warning',
+                                    'danger' => 'danger',
+                                    'info' => 'info',
+                                    default => null,
+                                };
+                            }
+                            if ($warnArray['warningContent']) {
+                                $warning['warningContent'] = $warnArray['warningContent'];
+                            }
+                            if (empty($warnArray['warningLevel']) || empty($warnArray['warningContent'])) {
+                                $warning['warningLevel'] = null;
+                                $warning['warningContent'] = null;
+                            }
+                        }
+                    }
+                    $htMTime = gmdate('D M d Y', strtotime($mtime));
+                    $htmlTimeTag = "<time datetime=$mtime data-tolocaltime=date-only>$htMTime</time>";
+                    if (!empty($warning['warningLevel'])) {
+                        $warningHTML = "<div class='warnbox {$warning['warningLevel']}'><p><strong>{$warning['warningLevel']}!</strong> {$warning['warningContent']}</div>";
+                    } else {
+                        $warningHTML = '<p>No applicableWarning';
+                    }
+                    $result .= "<tr><td><a href=$slugHTML/$versionHTML/>$versionHTML</a><td>$htmlTimeTag<td>$warningHTML";
+                    $result .= "<td><a download=$slugHTML/$versionHTML.txt href=$slugHTML/$versionHTML/>$versionHTML</a>";
                 }
-                $result .= "</ul>";
+                $result .= "</tbody></table></div>";
             }
             return $result;
         })() ?></div>
+    <!--<div><?= (function () use ($array) {
+        $result = '';
+        foreach ($array as $slug => $item) {
+            $slugHTML = htmlspecialchars12($slug);
+            $result .= "<h3 id=\"$slugHTML\">$slugHTML</h3><ul>";
+            foreach ($item as $version) {
+                $versionHTML = "{$version['major']}.{$version['minor']}.{$version['patch']}";
+                $result .= "<li><a href=$slugHTML/$versionHTML/>$versionHTML</a>";
+            }
+            $result .= "</ul>";
+        }
+        return $result;
+    })() ?></div>-->
     <!--<h2>Debug Data</h2><pre><code>&lt;?= htmlspecialchars12(json_fromArray(['$array' => $array])) ?></code></pre>-->
 </div>
